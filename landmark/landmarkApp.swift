523b63a234abeb36ba85185eecfed6a7085ef3d3
Jakubpatelski <jakubpatelski24@gmail.com>
2023-01-13 12:06:59 +0100

    Initial Commit

---
+++ landmark/landmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkApp.swift
+//  landmark
+//
+//  Created by Jakub Patelski on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct landmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
